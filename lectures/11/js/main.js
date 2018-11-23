97fe7f31f4b0b8a037cdf8b8fdb9dab20fef794b
Faiq Ali <faiqkhan88@ucp.edu.pk>
2018-11-23 20:07:51 +0500

    Done Activity(HomeworkTasks)

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -183,7 +183,107 @@ HINT: use text-decoration:line-through; property of CSS
 
 2- Enable Editing todos in text field to update text
 
-
-
 */
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' +'<p  id="\'+i+\'" onclick="striketext(this)">'+ todos[i].task +'</p>' +'</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>'+
+             '<button class="btn btn-Edit" id1="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Edit </span> </button>';
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    var buttons2 = document.getElementsByClassName('btn-Edit');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+        buttons2[i].addEventListener('click',editValue);
+    }
+}
+
+function editValue(){
+    var id = this.getAttribute('id1');
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.splice(id,1);
+    todos.push({task: task, isDone: false});
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+
+function striketext(obj)
+{
+         var a = obj.style.textDecoration ="line-through";
+}
+
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
+
+
+
 
